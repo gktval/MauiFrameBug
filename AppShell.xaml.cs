a3e951f1dd8c956fbe55e4538ff7cac178f652bc
Travis Yeik <travisyeik@outlook.com>
2024-04-24 10:30:39 -0600

    initial

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppFrameBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiAppFrameBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<MyListModel> ItemSource { get; set; }
+        public MyListModel SelectedItem { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ItemSource =
+            [
+                new MyListModel("this", 123),
+                new MyListModel("that", 456),
+                new MyListModel("then some", 789),
+            ];
+
+            BindingContext = this;
+        }
+
+
+    }
+
+
+    public class MyListModel
+    {
+        public MyListModel(string v1, int v2)
+        {
+            Name = v1;
+            Value = v2;
+        }
+
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+    }
+}
